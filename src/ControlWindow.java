e85eea9abdd15562833bd4039e89fd4ba533049b
denisoidea <denisoidea@gmail.com>
2016-11-20 16:21:17 -0600

    Added ControlWindow.java

---
+++ src/ControlWindow.java
@@ -0,0 +1,25 @@
+
+import processing.core.PApplet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daudirac
+ */
+public class ControlWindow extends PApplet {
+
+  public void settings() {
+    size(200, 100);
+  }
+  public void draw() {
+    background(255);
+    fill(0);
+    ellipse(100, 50, 10, 10);
+  }
+}
+
