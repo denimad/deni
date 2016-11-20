69ad8c28b136ea246df7f53796e3593dc7a6f5a4
denisoidea <denisoidea@gmail.com>
2016-11-20 16:31:39 -0600

    Added simple object test sketches.

---
+++ src/SimpleFan.java
@@ -0,0 +1,36 @@
+
+import Pattern.FanPattern;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daudirac
+ */
+public class SimpleFan extends ProcessingSketch
+{
+    public void settings(){
+        super.settings();
+        fan = new FanPattern();
+        this.addMouseListenerObject(fan);
+    }
+
+    @Override
+    public void setup() 
+    {
+        
+    }
+    
+    @Override
+    public void draw()
+    {
+        fan.draw();
+    }
+    
+    FanPattern fan;
+    
+}
---
+++ src/SimpleStroke.java
@@ -0,0 +1,41 @@
+
+import Pencil.Stroke;
+import Pencil.StrokeFollower2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daudirac
+ */
+public class SimpleStroke extends ProcessingSketch
+{
+
+    @Override
+    public void settings(){
+        super.settings();
+        strokeElement = new Stroke();
+        this.addMouseListenerObject(strokeElement);
+    }
+
+    @Override
+    public void setup() 
+    {
+        
+    }
+    
+    @Override
+    public void draw()
+    {
+        strokeElement.drawSequence();
+   
+    }
+    
+    
+    Stroke strokeElement;
+   
+}
